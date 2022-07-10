5d55154c36fa1eb54ab40351bd7a87d4f7dc9f88
NonnyJay <chinonso.aguonye@eng.uniben.edu>
2022-07-10 19:00:15 +0100

    Feat: All backend features completed

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,8 +1,12 @@
-import os
+#from crypt import methods
+import os, sys, ssl
+import re
 from flask import Flask, request, jsonify, abort
-from sqlalchemy import exc
+from sqlalchemy import desc, exc, func
 import json
 from flask_cors import CORS
+import werkzeug
+
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
@@ -17,7 +21,7 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+#db_drop_and_create_all()
 
 # ROUTES
 '''
@@ -28,7 +32,39 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks')
+def retrieve_drinks():
+    try:
+        # Fetach all drinks in the short format and return them
+        drinks_query = Drink.query.order_by('id').all()
 
+        if len(drinks_query) == 0:
+            raise werkzeug.exceptions.NotFound
+        """ print("check 3")
+        print(drinks_query[0])
+        print(drinks_query[0].id)
+        print(drinks_query[0].title)
+        print(drinks_query[0].recipe)
+        print(type(drinks_query[0]))
+        print(type(drinks_query))
+        data = {
+            "id": drinks_query[0].id,
+            "title": drinks_query[0].title,
+            "recipe": drinks_query[0].recipe
+        } """
+        data = [drink.short() for drink in drinks_query]
+        #print(data)
+        # Parse response data in the expected json format
+        return jsonify({
+            'success': True,
+            'drinks': data
+        })
+    except werkzeug.exceptions.NotFound:
+            print(sys.exc_info())
+            abort(404)
+    except:
+        print(sys.exc_info())
+        abort(500)
 
 '''
 @TODO implement endpoint
@@ -38,7 +74,29 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks-detail')
+@requires_auth('get:drinks-detail')
+def retrieve_drinks_details():
+    try:
+        # Fetch all drinks in the long format and return them
+        drinks_query = Drink.query.order_by('id').all()
 
+        if len(drinks_query) == 0:
+            raise werkzeug.exceptions.NotFound
+
+        data = [drink.long() for drink in drinks_query]
+        #print(data)
+        # Parse response data in the expected json format
+        return jsonify({
+            'success': True,
+            'drinks': data
+        })
+    except werkzeug.exceptions.NotFound:
+            print(sys.exc_info())
+            abort(404)
+    except:
+        print(sys.exc_info())
+        abort(500)
 
 '''
 @TODO implement endpoint
@@ -49,7 +107,48 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def create_drinks():
+    try:
+        body = request.get_json()
+        new_title = body.get('title', None)
+        new_recipe = body.get('recipe', None)
+        
+        # Validating data on the payload
+        if (new_title and new_recipe) is None:
+            raise werkzeug.exceptions.BadRequest
+        elif len(new_recipe) == 1:
+            if len(new_recipe[0]) < 3:
+                raise werkzeug.exceptions.BadRequest
+        elif len(new_recipe) > 1:
+            for rec in new_recipe:
+                if len(rec) < 3:
+                    raise werkzeug.exceptions.BadRequest
 
+        json_rec = json.dumps(new_recipe)
+        #print(json_rec)
+
+        # Persist record to the database
+        add_drinks = Drink(
+            title = new_title,
+            recipe = json_rec
+        )
+        add_drinks.insert()
+        #print(add_drinks)
+
+        data = add_drinks.long()
+        #print(data)
+        return jsonify({
+            "success": True,
+            "drinks": data
+        })
+    except werkzeug.exceptions.BadRequest:
+        print(sys.exc_info())
+        abort(400)
+    except:
+        print(sys.exc_info())
+        abort(500)
 
 '''
 @TODO implement endpoint
@@ -62,7 +161,61 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def modify_drinks(id):
+    
+    try:
+        # check if drink with id exists on the database
+        drinks_query = Drink.query.filter(Drink.id == id).one_or_none()
+        if drinks_query is None:
+            raise werkzeug.exceptions.NotFound
 
+        # Extract dat from the json object
+        body = request.get_json()
+        update_title = body.get('title', None)
+        update_recipe = body.get('recipe', None)
+
+        # Validating data on the payload
+        if update_recipe is not None:
+            if len(update_recipe) == 1:
+                if len(update_recipe[0]) < 3:
+                    raise werkzeug.exceptions.BadRequest
+            else:
+                for rec in update_recipe:
+                    if len(rec) < 3:
+                        raise werkzeug.exceptions.BadRequest
+
+            #Convert list to json object
+            json_rec = json.dumps(update_recipe)
+            #print(json_rec)
+
+            drinks_query.recipe = json_rec
+
+        if update_title is not None:
+            drinks_query.title = update_title
+
+        if update_title is None and update_recipe is None:
+            raise werkzeug.exceptions.BadRequest
+
+        # Persist changes to the database
+        drinks_query.update()
+
+        data = drinks_query.long()
+
+        return jsonify({
+            "success": True,
+            "drinks": data
+        })
+    except werkzeug.exceptions.BadRequest:
+        print(sys.exc_info())
+        abort(400)
+    except werkzeug.exceptions.NotFound:
+        print(sys.exc_info())
+        abort(404)
+    except:
+        print(sys.exc_info())
+        abort(500)
 
 '''
 @TODO implement endpoint
@@ -74,13 +227,34 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drinks(id):
+    try:
+        # check if drink with id exists on the database
+        del_query = Drink.query.filter(Drink.id == id).one_or_none()
+        if del_query is None:
+            raise werkzeug.exceptions.NotFound
 
+        # Persist delete to the database
+        del_query.delete()
+
+        return jsonify({
+            "success": True,
+            "delete": id
+        })
+
+    except werkzeug.exceptions.NotFound:
+        print(sys.exc_info())
+        abort(404)
+    except:
+        print(sys.exc_info())
+        abort(500)
 
 # Error Handling
 '''
 Example error handling for unprocessable entity
 '''
-
 
 @app.errorhandler(422)
 def unprocessable(error):
@@ -94,21 +268,78 @@ def unprocessable(error):
 '''
 @TODO implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
+    jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+        }), 404
 '''
+"""
+This session handles all error to JSON format.
+"""
+@app.errorhandler(401)
+def unauthorize_error(error):
+    return jsonify({
+        'success' : False,
+        'error' : 401,
+        'message' : "You do not possess the required access"
+    }), 401
+
+@app.errorhandler(403)
+def forbidden_error(error):
+    return jsonify({
+        'success' : False,
+        'error' : 403,
+        'message' : "Forbidden: Access denied"
+    }), 403
+
+@app.errorhandler(400)
+def bad_request_error(error):
+    return jsonify({
+        'success' : False,
+        'error' : 400,
+        'message' : "The request was invalid"
+    }), 400
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({
+        'success' : False,
+        'error' : 500,
+        'message' : "Server Error has occured"
+    }), 500
+
+@app.errorhandler(405)
+def method_not_allowed_error(error):
+    return jsonify({
+        'success' : False,
+        'error' : 405,
+        'message' : "Request Method not Allowed"
+    }), 405
 
 '''
 @TODO implement error handler for 404
     error handler should conform to general task above
 '''
-
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({
+        'success' : False,
+        'error' : 404,
+        'message' : "The request is not found"
+    }), 404
 
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+@app.errorhandler(AuthError)
+def authorization_header_missing(error):
+    #print(error)
+    #print(error.error['description'])
+    #print(error.status_code)
+    return jsonify({
+        'success' : False,
+        'error' : error.status_code,
+        'message' : error.error['description']
+    }), 403
--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_filename = "database.db"
+database_filename = "dev_database.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
 
@@ -28,7 +28,6 @@ db_drop_and_create_all()
     can be used to initialize a clean database
     !!NOTE you can change the database_filename variable to have multiple verisons of a database
 '''
-
 
 def db_drop_and_create_all():
     db.drop_all()
